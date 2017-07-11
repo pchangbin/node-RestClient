e8987ed065987aa5ee6affa50dd6b48334e3bde8
Changbin Park <changbin.park@intel.com>
2017-07-11 22:33:00 +0900

    Add base test framework and lint environment

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 8,
+  },
+  rules: {
+    indent:             [ 'error', 2 , {SwitchCase: 1}],
+    'linebreak-style':  [ 'error', 'unix' ],
+    quotes:             [ 'error', 'single' ],
+    semi:               [ 'error', 'always' ],
+    'no-console':       'off',
+    'no-unused-vars':   'warn',
+  }
+};
