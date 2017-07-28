d19af02d6c796a8cac51e56edd73fc76a5e5601a
Changbin Park <changbin.park@intel.com>
2017-07-28 15:55:46 +0900

    Regroup testcases

--- test/test.js
+++ test/test.js
@@ -22,7 +22,7 @@ describe('Instantication', function() {
   });
 });
 
-describe('RestApiClient method', function() {
+describe('RestApiClient', function() {
   this.timeout(5000);
 
   let client;
@@ -30,47 +30,70 @@ describe('RestApiClient method', function() {
     client = new RestApiClient({url});
   });
 
-  describe('post', function() {
-    const path = '/post';
-    it('send/receive JSON object', async function() {
-      const body = {foo:'hello', bar:'world'};
-      const {json} = await client.post(path, body);
-      assert.deepEqual(body, json);
-    });
-    it('send/receive string', async function() {
-      const body = 'Hello world!';
-      const {data} = await client.post(path, body);
-      assert.strictEqual(body, data);
-    });
-  });
-
-  describe('get', function() {
-    it('pass given object as search parameters', async function() {
-      const params = {
-        foo: 'hello',
-        bar: 'world',
-      };
-      const resp = JSON.parse(await client.get('/get', params));
-      assert.deepEqual(params, resp.args);
-    });
-
-    it('merges search parameters in aPath and given parameter as object',
-      async function() {
+  describe('Basic HTTP method', function() {
+    describe('get', function() {
+      it('pass given object as search parameters', async function() {
         const params = {
           foo: 'hello',
           bar: 'world',
         };
-        const resp = JSON.parse(await client.get('/get?baz', params));
-        assert.strictEqual(resp.args.foo, params.foo);
-        assert.strictEqual(resp.args.bar, params.bar);
-        assert.strictEqual(resp.args.baz, '');
+        const resp = JSON.parse(await client.get('/get', params));
+        assert.deepEqual(params, resp.args);
       });
+
+      it('merges search parameters in aPath and given parameter as object',
+        async function() {
+          const params = {
+            foo: 'hello',
+            bar: 'world',
+          };
+          const resp = JSON.parse(await client.get('/get?baz', params));
+          assert.strictEqual(resp.args.foo, params.foo);
+          assert.strictEqual(resp.args.bar, params.bar);
+          assert.strictEqual(resp.args.baz, '');
+        });
+    });
+
+    describe('post', function() {
+      const path = '/post';
+      it('send/receive JSON object', async function() {
+        const body = {foo:'hello', bar:'world'};
+        const {json} = await client.post(path, body);
+        assert.deepEqual(body, json);
+      });
+      it('send/receive string', async function() {
+        const body = 'Hello world!';
+        const {data} = await client.post(path, body);
+        assert.strictEqual(body, data);
+      });
+    });
+
+    describe.skip('head', function() {
+      it('TODO Implement', function() {
+      });
+    });
+    describe.skip('delete', function() {
+      it('TODO Implement', function() {
+      });
+    });
+    describe.skip('options', function() {
+      it('TODO Implement', function() {
+      });
+    });
+    describe.skip('put', function() {
+      it('TODO Implement', function() {
+      });
+    });
+    describe.skip('patch', function() {
+      it('TODO Implement', function() {
+      });
+    });
   });
 
   describe('HTTP Header functionality', function() {
     const key = 'Foo', value = 'hello WORLD';
     describe('property "headers"', function() {
-      it('permanent headers are exposed through "hearers" property', function () {
+      it('exposes permanent headers', function () {
         const {headers} = client;
         assert.ok(headers.accept);
         assert.ok(headers['content-type']);
@@ -81,6 +104,7 @@ describe('RestApiClient method', function() {
         assert.strictEqual(client.headers[key], value);
       });
     });
+
     describe('setHeader', function() {
       before('Clear HTTP header for test from client instance', function() {
         delete client.headers[key];
@@ -99,23 +123,25 @@ describe('RestApiClient method', function() {
 });
 
 describe('Static method', function() {
-  describe('post', function() {
-    const full_url = `${url}/post`;
-    it('send/receive JSON object', async function() {
-      const body = {foo:'hello', bar:'world'};
-      const {json} = await RestApiClient.post(full_url, body);
-      assert.deepEqual(body, json);
+  describe('Basic HTTP method', function() {
+    describe('post', function() {
+      const full_url = `${url}/post`;
+      it('send/receive JSON object', async function() {
+        const body = {foo:'hello', bar:'world'};
+        const {json} = await RestApiClient.post(full_url, body);
+        assert.deepEqual(body, json);
+      });
     });
-  });
 
-  describe('get', function() {
-    it('pass given object as search parameters', async function() {
-      const params = {
-        foo: 'hello',
-        bar: 'world',
-      };
-      const resp = JSON.parse(await RestApiClient.get(`${url}/get`, params));
-      assert.deepEqual(params, resp.args);
+    describe('get', function() {
+      it('pass given object as search parameters', async function() {
+        const params = {
+          foo: 'hello',
+          bar: 'world',
+        };
+        const resp = JSON.parse(await RestApiClient.get(`${url}/get`, params));
+        assert.deepEqual(params, resp.args);
+      });
     });
   });
 });
